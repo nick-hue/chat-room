92d711d1ae7250bf5b198c26f58fa1e41dcf58ad
nick-hue <nickagelid@gmail.com>
2024-02-03 18:09:19 +0200

    added git ignore

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
 from tkinter import *
-from tkinter import filedialog
 import customtkinter as ctk
-import controller
 from PIL import Image
+import server 
+import client
+
+# FONTS
+BUTTON_FONT_BOLD=('Open Sans', 32, 'bold')
+
+# WIDGET ATTRIBUTES
+BUTTON_HEIGHT=100
+BUTTON_WIDTH=200
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
 
         self.title("Chat Room")
-        self.geometry("400x650")
+        self.geometry("800x400")
         self.resizable(False, False)
 
-        self.grid_rowconfigure((0,1,2), weight=1)
+        self.host_button = ctk.CTkButton(self, text="HOST", height=BUTTON_HEIGHT, width=BUTTON_WIDTH, font=BUTTON_FONT_BOLD, command=self.host)
+        self.host_button.grid(row=0, column=0, padx=100, pady=150)
 
+        self.join_button = ctk.CTkButton(self, text="JOIN", height=BUTTON_HEIGHT, width=BUTTON_WIDTH, font=BUTTON_FONT_BOLD, command=self.join)
+        self.join_button.grid(row=0, column=1, padx=100, pady=150)
+
+    def host(self):
+        pass
+
+    def join(self):
+        pass
 
 
 if __name__ == "__main__":
