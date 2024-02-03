6bb36efd57a1d831407c467ac8dea4a1cf4b6018
nick-hue <nickagelid@gmail.com>
2024-02-03 18:19:13 +0200

    started chat room

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import client
 
 # FONTS
 BUTTON_FONT_BOLD=('Open Sans', 32, 'bold')
+ENTRY_FONT_BOLD=('Open Sans', 26, 'bold')
+
 
 # WIDGET ATTRIBUTES
 BUTTON_HEIGHT=100
@@ -19,14 +21,31 @@ class App(ctk.CTk):
         self.geometry("800x400")
         self.resizable(False, False)
 
-        self.host_button = ctk.CTkButton(self, text="HOST", height=BUTTON_HEIGHT, width=BUTTON_WIDTH, font=BUTTON_FONT_BOLD, command=self.host)
-        self.host_button.grid(row=0, column=0, padx=100, pady=150)
+        self.main_frame = ctk.CTkFrame(self, corner_radius=0)
+        self.main_frame.grid(row=0, column=0, sticky=NSEW)
 
-        self.join_button = ctk.CTkButton(self, text="JOIN", height=BUTTON_HEIGHT, width=BUTTON_WIDTH, font=BUTTON_FONT_BOLD, command=self.join)
-        self.join_button.grid(row=0, column=1, padx=100, pady=150)
+        self.host_button = ctk.CTkButton(self.main_frame, text="HOST", height=BUTTON_HEIGHT, width=BUTTON_WIDTH, font=BUTTON_FONT_BOLD, command=self.host)
+        self.host_button.grid(row=0, column=0, padx=100, pady=150, sticky=NSEW)
+
+        self.join_button = ctk.CTkButton(self.main_frame, text="JOIN", height=BUTTON_HEIGHT, width=BUTTON_WIDTH, font=BUTTON_FONT_BOLD, command=self.join)
+        self.join_button.grid(row=0, column=1, padx=100, pady=150, sticky=NSEW)
 
     def host(self):
-        pass
+        self.main_frame.grid_forget()
+
+        self.chat_name_var = ctk.StringVar()
+
+        ctk.CTkLabel(self, text="Chat room name: ").grid(row=0, column=0, padx=20, pady=100, sticky=NSEW)
+        self.chat_name_entry = ctk.CTkEntry(self, textvariable=self.chat_name_var, font=ENTRY_FONT_BOLD)
+        self.chat_name_entry.grid(row=0, column=1, padx=20, pady=10, sticky=NSEW)
+
+        ctk.CTkLabel(self, text="IP ADDRESS: ").grid(row=1, column=0, padx=20, pady=100, sticky=NSEW)
+        self.ip_address_entry = ctk.CTkEntry(self, textvariable=self.chat_name_var, font=ENTRY_FONT_BOLD)
+        self.ip_address_entry.grid(row=1, column=1, padx=20, pady=10, sticky=NSEW)
+
+        ctk.CTkLabel(self, text="PORT: ").grid(row=2, column=0, padx=20, pady=100, sticky=NSEW)
+        self.port_number_entry = ctk.CTkEntry(self, textvariable=self.chat_name_var, font=ENTRY_FONT_BOLD)
+        self.port_number_entry.grid(row=2, column=1, padx=20, pady=10, sticky=NSEW)
 
     def join(self):
         pass
--- server.py
+++ server.py
@@ -1,65 +1,66 @@
 import socket
 import select
 
-IP_ADDRESS = '127.0.0.1'  # LocalHost
-PORT = 12345  # Arbitrary non-privileged port
-MAX_CONNECTIONS = 2
+def host_connection(IP_ADDRESS, PORT, MAX_CONNECTIONS):
+    IP_ADDRESS = '127.0.0.1'  # LocalHost
+    PORT = 12345  # Arbitrary non-privileged port
+    MAX_CONNECTIONS = 2
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind((IP_ADDRESS, PORT))
-server_socket.listen(MAX_CONNECTIONS)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((IP_ADDRESS, PORT))
+    server_socket.listen(MAX_CONNECTIONS)
 
-sockets_list = [server_socket]
-clients = {}
+    sockets_list = [server_socket]
+    clients = {}
 
-print(f'Listening for connections on {IP_ADDRESS}:{PORT}...')
+    print(f'Listening for connections on {IP_ADDRESS}:{PORT}...')
 
-def receive_message(client_socket):
-    try:
-        message_header = client_socket.recv(10)
-        if not len(message_header):
+    def receive_message(client_socket):
+        try:
+            message_header = client_socket.recv(10)
+            if not len(message_header):
+                return False
+            message_length = int(message_header.decode('utf-8').strip())
+            return {"header": message_header, "data": client_socket.recv(message_length)}
+        except:
             return False
-        message_length = int(message_header.decode('utf-8').strip())
-        return {"header": message_header, "data": client_socket.recv(message_length)}
-    except:
-        return False
 
-while True:
-    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
-    
-    for notified_socket in read_sockets:
-        if notified_socket == server_socket:
-            client_socket, client_address = server_socket.accept()
-            
-            if len(sockets_list) - 1 < MAX_CONNECTIONS:  # -1 for the server socket itself
-                user = receive_message(client_socket)
-                if user is False:
+    while True:
+        read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+        
+        for notified_socket in read_sockets:
+            if notified_socket == server_socket:
+                client_socket, client_address = server_socket.accept()
+                
+                if len(sockets_list) - 1 < MAX_CONNECTIONS:  # -1 for the server socket itself
+                    user = receive_message(client_socket)
+                    if user is False:
+                        continue
+                    
+                    sockets_list.append(client_socket)
+                    clients[client_socket] = user
+                    print('Accepted new connection from {}:{}, username: {}'.format(*client_address, user['data'].decode('utf-8')))
+                else:
+                    print('Max connections reached. Connection refused.')
+                    client_socket.close()
+                    
+            else:
+                message = receive_message(notified_socket)
+                
+                if message is False:
+                    print('Closed connection from: {}'.format(clients[notified_socket]['data'].decode('utf-8')))
+                    sockets_list.remove(notified_socket)
+                    del clients[notified_socket]
                     continue
                 
-                sockets_list.append(client_socket)
-                clients[client_socket] = user
-                print('Accepted new connection from {}:{}, username: {}'.format(*client_address, user['data'].decode('utf-8')))
-            else:
-                print('Max connections reached. Connection refused.')
-                client_socket.close()
+                user = clients[notified_socket]
+                print(f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
                 
-        else:
-            message = receive_message(notified_socket)
-            
-            if message is False:
-                print('Closed connection from: {}'.format(clients[notified_socket]['data'].decode('utf-8')))
-                sockets_list.remove(notified_socket)
-                del clients[notified_socket]
-                continue
-            
-            user = clients[notified_socket]
-            print(f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
-            
-            for client_socket in clients:
-                if client_socket != notified_socket:
-                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
-    
-    for notified_socket in exception_sockets:
-        sockets_list.remove(notified_socket)
-        del clients[notified_socket]
+                for client_socket in clients:
+                    if client_socket != notified_socket:
+                        client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+        
+        for notified_socket in exception_sockets:
+            sockets_list.remove(notified_socket)
+            del clients[notified_socket]
